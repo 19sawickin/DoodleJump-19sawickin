62599dd0d0979b358586de345b401d38fc813be3
Nick Sawicki <19sawickin@nicks-air-3.devices.brown.edu>
2021-03-16 16:30:01 -0400

    platform shows up

--- Game.java
+++ Game.java
@@ -1,22 +1,36 @@
 package doodlejump;
 
+import cartoon.Cartoon;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
+import javafx.util.Duration;
 
 public class Game {
     private Doodle _doodle;
     private Platform _platform;
 
     public Game(BorderPane root, HBox _bottomPane, Pane doodlePane) {
+        this.setupTimeline();
         _doodle = new Doodle(doodlePane);
+        _platform = new Platform(root);
         new MoveHandler();
         root.setCenter(doodlePane);
         root.setBottom(_bottomPane);
         doodlePane.addEventHandler(KeyEvent.KEY_PRESSED, new KeyHandler());
         doodlePane.setFocusTraversable(true);
         doodlePane.requestFocus();
+    }
+
+    public void setupTimeline() {
+        Timeline timeline = new Timeline
+                (new KeyFrame(Duration.seconds(0.1), new MoveHandler()));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
     }
 
     private class KeyHandler implements EventHandler<KeyEvent> {
--- Platform.java
+++ Platform.java
@@ -1,8 +1,22 @@
 package doodlejump;
 
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
+
 public class Platform {
+    private static Rectangle _platform;
 
-    public void Platform() {
+    public Platform(BorderPane root) {
+        this.setupShape();
+        root.getChildren().add(_platform);
 
+    }
+
+    public void setupShape() {
+        _platform = new Rectangle(Constants.PLATFORM_WIDTH, Constants.PLATFORM_HEIGHT);
+        _platform.setFill(Color.GREEN);
+        _platform.setX(Constants.SCENE_WIDTH/2);
+        _platform.setY(Constants.SCENE_HEIGHT-Constants.PLATFORM_HEIGHT);
     }
 }
